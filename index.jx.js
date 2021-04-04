3c32a6dc6bbb04cb705f787e880849a1dac53818
purplereign09 <62455999+purplereign09@users.noreply.github.com>
2021-04-04 16:57:21 -0500

    Add files via upload

---
+++ index.jx.js
@@ -0,0 +1,7 @@
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
+var yellow = rootStyles.getPropertyValue('--yellow')
+console.log('yellow: ', yellow);
+
+
+root.style.setProperty('--yellow', 'blue');
